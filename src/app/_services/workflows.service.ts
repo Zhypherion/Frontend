32e83e6a21d760ea743e4ee0820520782f16c78e
Zhypherion <luckybow1232@gmail.com>
2025-10-04 11:08:50 +0800

    Added new link for transfer and workflows service

--- src/app/_services/workflows.service.ts
+++ src/app/_services/workflows.service.ts
@@ -5,7 +5,7 @@ import { Workflow } from '@app/_models/workflows';
 
 @Injectable({ providedIn: 'root' })
 export class WorkflowsService {
-  private apiUrl = 'http://localhost:4000/workflows';
+  private apiUrl = 'https://api-1-71ag.onrender.com/workflows';
 
   constructor(private http: HttpClient) {}
 
